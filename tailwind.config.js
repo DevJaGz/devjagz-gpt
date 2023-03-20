bf080e579283b8bb6e81f88679e3240bd1b9ece2
Julian Gomez <jgomez109@unab.edu.co>
2023-03-20 00:08:52 -0500

    Fix responsivitiy of sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    ...defaultTheme,
     colors: {
       ...colors,
-      sky: colors.lightBlue, // update lightBlue to sky
-      stone: colors.warmGray, // update warmGray to stone
-      neutral: colors.trueGray, // update trueGray to neutral
+      // sky: colors.lightBlue, // update lightBlue to sky
+      // stone: colors.warmGray, // update warmGray to stone
+      // neutral: colors.trueGray, // update trueGray to neutral
       "cs-dark": {
         DEFAULT: "#202123",
         800: "#2A2B32",
         700: "#353740",
         600: "#565869",
         500: "#40414F",
+        400: "#444654",
+        300: "#343541",
       },
       "cs-light": {
         DEFAULT: "#d9d9e3",
@@ -34,6 +38,11 @@ module.exports = {
         50: "0.5",
         25: "0.25",
         0: "0",
+      },
+      neutral: {
+        light: "#f2f2f2", // reemplaza con tu color neutral para el tema claro
+        DEFAULT: "#6B7280", // reemplaza con tu color neutral predeterminado
+        dark: "#1f2937", // reemplaza con tu color neutral para el tema oscuro
       },
     },
     extend: {},
