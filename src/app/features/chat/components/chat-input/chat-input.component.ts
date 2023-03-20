d9863022d4ac372e1b201e3612bf8e1755e6d766
Julian Gomez <jgomez109@unab.edu.co>
2023-03-19 22:50:41 -0500

    Add logic to send the value of the textarea

--- src/app/features/chat/components/chat-input/chat-input.component.ts
+++ src/app/features/chat/components/chat-input/chat-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-chat-input',
@@ -6,17 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat-input.component.scss'],
 })
 export class ChatInputComponent {
+  @Output() onNewValue = new EventEmitter<string>();
+
   readonly minHeight = 24;
   readonly maxHeight = 200;
 
-  onTextareaInput(textarea: HTMLTextAreaElement) {
+  onInputTextarea(textarea: HTMLTextAreaElement): void {
     textarea.style.height = 'auto';
     const newHeight = Math.max(textarea.scrollHeight, this.minHeight);
     textarea.style.height = `${newHeight}px`;
-    // if (newHeight <= this.maxHeigth) {
-    //   textarea.style.height = `${newHeight}px`;
-    // } else {
-    //   textarea.style.height = `${this.maxHeigth}px`;
-    // }
+  }
+
+  onKeydown(event: KeyboardEvent): void {
+    const { key, target, shiftKey } = event;
+    const textarea = target as HTMLTextAreaElement;
+    const isEnterPressed = key === 'Enter';
+    const isSubmiting = !shiftKey && isEnterPressed;
+    if (isSubmiting) {
+      event.preventDefault();
+      this.sendValue(textarea);
+    }
+  }
+
+  onSendClicked(textarea: HTMLTextAreaElement): void {
+    this.sendValue(textarea);
+  }
+
+  private sendValue(textarea: HTMLTextAreaElement): void {
+    const { style, value } = textarea;
+    this.onNewValue.emit(value);
+    textarea.value = '';
+    style.height = `${this.minHeight}px`;
+    textarea.focus();
   }
 }
